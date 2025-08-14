b2bfaf786d0a64b770bfa040384ed79514275ad0
panshor <juanjodc79@gmail.com>
2025-08-13 19:04:21 -0500

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,32 @@
+package ejercicio2;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int sumapar = 0;
+        int producto = 0;
+       
+        
+        for (int n = 20; n < 400; n++) {
+            if (n%2==0){
+                sumapar = sumapar+n;
+                producto = n*n;
+           
+                
+            }else{
+                
+            }
+            
+                
+            }
+            System.out.println("La suma de los pares es: "+sumapar);
+            System.out.println("El producto es: "+producto);
+            
+            
+        
+     
+
+
+    }
+    
+}
